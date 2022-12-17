f3e2cbfbcce6a8f876ea70880094f73d7e900e46
Danila Safonov <danila24safonov@mail.ru>
2022-12-17 18:32:10 +0300

    use attributes

--- MoviesApp/Filters/ActorsNameAttribute.cs
+++ MoviesApp/Filters/ActorsNameAttribute.cs
@@ -3,13 +3,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MoviesApp.Filters {
     public class ActorsNameAttribute : ValidationAttribute {
-        public string Name { get; }
-        public ActorsNameAttribute(string name) {
-            Name = name;
-        }
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext) {
+        protected override ValidationResult IsValid(object name, ValidationContext validationContext) {
 
-            if (Name.Length < 4) {
+            if (((String)name).Length < 4) {
                 return new ValidationResult("This parameter cannot be less tan 4 symbols");
             }
 
--- MoviesApp/ViewModels/InputActorViewModel.cs
+++ MoviesApp/ViewModels/InputActorViewModel.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using MoviesApp.Filters;
+using System;
 
 namespace MoviesApp.ViewModels {
 	public class InputActorViewModel {
+		[ActorsName]
 		public string FirstName { get; set; }
+		[ActorsName]
 		public string LastName { get; set; }
 		public DateTime BirthDay { get; set; }
 	}
